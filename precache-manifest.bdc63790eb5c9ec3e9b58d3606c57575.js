d88eb1c3bc6343d8ac6fa19b9f02616c70e8a23e
Roman Galaniuk <romangalanuk@gmail.com>
2020-03-24 03:52:22 +0200

    Updates

---
+++ precache-manifest.bdc63790eb5c9ec3e9b58d3606c57575.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7459c67a08678b97722a7c202e3a7ad",
+    "url": "/breakingNews/index.html"
+  },
+  {
+    "revision": "39fc2e3097f868a67ce1",
+    "url": "/breakingNews/static/css/main.45e571b5.chunk.css"
+  },
+  {
+    "revision": "24358f59b5b0cb42f89f",
+    "url": "/breakingNews/static/js/2.620b937c.chunk.js"
+  },
+  {
+    "revision": "eb56b19eba63a53416f4fd778023529f",
+    "url": "/breakingNews/static/js/2.620b937c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39fc2e3097f868a67ce1",
+    "url": "/breakingNews/static/js/main.28ff2ccd.chunk.js"
+  },
+  {
+    "revision": "e6349d8df61e47ac0dfc",
+    "url": "/breakingNews/static/js/runtime-main.1fcddd60.js"
+  }
+]);
